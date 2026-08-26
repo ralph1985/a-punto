4c095db9df55c7a50b659e8e7633ab8b4f13458f
Rafael García <rafaelgarcia1985@hotmail.com>
2026-08-26 22:04:04 +0200

    fix: use Next TypeScript API during builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    useTypeScriptCli: false,
+  },
   async headers() {
     return [{
       source: "/(.*)",
